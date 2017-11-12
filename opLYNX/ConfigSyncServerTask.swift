8156a8c3f99eac19784f5b67e1fcb54993d0efb9
Ian Campbell <ianc@gyges2.local>
2017-11-12 13:42:11 -0700

    Updates to Config Sync

---
+++ opLYNX/ConfigSyncServerTask.swift
@@ -0,0 +1,20 @@
+//
+//  ConfigSyncServerTask.swift
+//  opLYNX
+//
+//  Created by Ian Campbell on 2017-11-12.
+//  Copyright © 2017 CIS. All rights reserved.
+//
+
+import Foundation
+
+class ConfigSyncServerTask: OPLYNXServerTask {
+    
+    // Inserts a "last_update" parameter to all the Task before calling Run
+    override func RunTask() {
+        if let lastConfigSyncDate = ConfigSync.ASSET_SOFTWARE_INFO?.LastSyncConfiguration {
+            addParameter(name: "last_update", value: "\"" + lastConfigSyncDate.formatJsonDate() + "\"")
+        }
+        super.RunTask()
+    }
+}
